7bde4494411c24f4b3ef245b7dcf0ca082a7a593
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-14 19:24:13 +0200

    Worcs log

--- src/components/ShowCharacters.jsx
+++ src/components/ShowCharacters.jsx
@@ -1,15 +1,19 @@
 import axios from "axios" 
 import { useEffect } from "react"
+import { useState } from "react"
 
 const url = "http://localhost:8080/characters" 
 const ShowCharacters = () => {
+   
+    const [characters, setCharacters] = useState ([])    
 useEffect( () => {
 getAllCharacters()
 }, [])
 const getAllCharacters = async () => { 
     const response = await axios.get(url) 
     let data = response.data
-   
+    console.log(response.data)
+    setCharacters(data)
 }
 
 return (
