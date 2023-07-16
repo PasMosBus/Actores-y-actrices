067a2127049ea6f765d95bf6490426430f4143b7
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-16 22:26:39 +0200

    Added page Edit

---
+++ src/pages/Edit.jsx
@@ -0,0 +1,15 @@
+import EditCharacters from "../components/EditCharacters";
+
+import '../pages/Edit.css'
+
+const Edit = () => {
+    return (
+        <>
+            
+            <div className="">
+                <EditCharacters />
+            </div>
+        </>
+    )
+}
+export default Edit;
