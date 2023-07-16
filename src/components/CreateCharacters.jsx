7fcd3e568c1a4bf21f8d17e06c0fc6851845ac7f
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-16 23:08:28 +0200

    Fixed css

--- src/components/CreateCharacters.jsx
+++ src/components/CreateCharacters.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import './EditCharacters.css'
 
 const url = "http://localhost:8080/characters"
 
@@ -16,30 +17,32 @@ const CreateCharacters = () => {
         e.preventDefault()
         // dos parametros: 1 = url, 2 = un objeto con mis datos.
         //Aquí le digo como enlazar eso datos de la API con los que le voy a enviar
-        await axios.post(url, { name: name, img: picture, description: discribe})
+        await axios.post(url, { name: name, img: picture, description: discribe })
         navigate("/")
     }
 
     return (
-        <><h3>Create an element</h3>
-        <form onSubmit={store}>
-            <div>
-                <form>Name</form>
-                {/* nombre tiene que valer lo que ingresamos en el valué del input */}
-                {/*  */}
-                <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>  
+        <>
+            <div className="container-create">
+                <h3>Create an element</h3>
+                <form onSubmit={store}>
+                    <div >
+                        <form>Name</form>
+                        {/* nombre tiene que valer lo que ingresamos en el valué del input */}
+                        {/*  */}
+                        <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div>
+                        <form>Picture</form>
+                        <input type="text" value={picture} onChange={(e) => setPicture(e.target.value)} />
+                    </div>
+                    <div>
+                        <form>Discribe</form>
+                        <textarea name="" id="" cols="22" rows="15" value={discribe} onChange={(e) => setDiscribe(e.target.value)}></textarea>
+                    </div>
+                </form>
+                <button className="buttons" type="submit">Add</button>
             </div>
-            <div>
-                <form>Picture</form>
-                <input type="text" value={picture} onChange={(e) => setPicture(e.target.value)}/>
-            </div>
-            <div>
-                <form>Discribe</form>
-                <textarea name="" id="" cols="22" rows="15" value={discribe} onChange={(e) => setDiscribe(e.target.value)}></textarea>
-            </div>
-            <button type="submit">Crear an card</button>
-        </form>
-        
         </>
     )
 }
--- src/components/EditCharacters.jsx
+++ src/components/EditCharacters.jsx
@@ -11,66 +11,67 @@ export default EditCharacters */
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import './EditCharacters.css'
 
 const EditCharacters = () => {
-  const { id } = useParams();
-  const navigate = useNavigate();
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  const [name, setName] = useState("");
-  const [picture, setPicture] = useState("");
-  const [discribe, setDescription] = useState("");
+    const [name, setName] = useState("");
+    const [picture, setPicture] = useState("");
+    const [discribe, setDescription] = useState("");
 
-  useEffect(() => {
-    fetchCharacter();
-  }, []);
+    useEffect(() => {
+        fetchCharacter();
+    }, []);
 
-  const fetchCharacter = async () => {
-    try {
-      const response = await axios.get(`http://localhost:8080/characters/${id}`);
-      const { name, picture, discribe } = response.data;
-      setName(name);
-      setPicture(picture);
-      setDescription(discribe);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const fetchCharacter = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/characters/${id}`);
+            const { name, picture, discribe } = response.data;
+            setName(name);
+            setPicture(picture);
+            setDescription(discribe);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  const updateCharacter = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.put(`http://localhost:8080/characters/${id}`, {
-        name,
-        picture,
-        discribe,
-      });
-      alert("Character updated successfully");
-      navigate("/");
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const updateCharacter = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.put(`http://localhost:8080/characters/${id}`, {
+                name,
+                picture,
+                discribe,
+            });
+            alert("Character updated successfully");
+            navigate("/");
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (
-    <div>
-      <h3>Edit Character</h3>
-      <form onSubmit={updateCharacter}>
-        <div>
-        <form>Name</form>
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+    return (
+        <div className="container-edit">
+            <h3>Edit Character</h3>
+            <form onSubmit={updateCharacter}>
+                <div>
+                    <form>Name</form>
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                </div>
+                <div>
+                    <form>Picture</form>
+                    <input type="text" value={picture} onChange={(e) => setPicture(e.target.value)} />
+                </div>
+                <div>
+                    <form>Discribe</form>
+                    <textarea name="" id="" cols="22" rows="15" value={discribe} onChange={(e) => setDiscribe(e.target.value)}></textarea>
+                </div>
+            </form>
+            <button className="buttons" type="submit">Update</button>
         </div>
-        <div>
-        <form>Picture</form>
-          <input type="text" value={picture} onChange={(e) => setPicture(e.target.value)}/>
-        </div>
-        <div>
-        <form>Discribe</form>
-          <textarea name="" id="" cols="22" rows="15" value={discribe} onChange={(e) => setDiscribe(e.target.value)}></textarea>
-        </div>
-        <button type="submit">Update Character</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default EditCharacters;
