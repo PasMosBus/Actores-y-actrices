b725e03fc914016fe6cc9e968194ac519d008df3
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-18 00:08:12 +0200

    fixed css

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter} from 'react-router-dom'
 import Home from '../pages/Home';
 import CreateCharacters from '../components/CreateCharacters';
 import EditCharacters from '../components/EditCharacters';
-import DeliteCharacters from '../components/DeliteCharacters';
 export const router = createBrowserRouter([
 
   
@@ -18,10 +17,7 @@ export const router = createBrowserRouter([
         path: "/edit/:id",
         element: <EditCharacters/>
       },
-      {
-        path: "/delite/:id",
-        element: <DeliteCharacters/>
-      },
+ 
    
   ]);
 
