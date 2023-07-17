7141ed5e7e45ba746ff3be41add09fec1c3bdb66
Viktor.A <viktor.antonyshyn@gmail.com>
2023-07-17 08:23:32 +0200

    fixed css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
     return (
         <>
             <div className="box">
-                <button className="buttons">Crear</button>
-                {/* <NavLink to="/create"> <button>Crear</button> </NavLink> */}
+                <button className="buttons">Create</button>
+                {/* <NavLink to="/create"> <button>Create</button> </NavLink> */}
                 <div className="cont">
                     <ShowCharacters />
                 </div>
